2b76cb514460b93a2d51906c87bdce304613b5b1
kanon-inoue <kanon.inoue.83@gmail.com>
2026-08-24 10:35:26 +0000

    removed unnesecarry imports

--- Restaurant-Booking-Management-Application/backend/src/controllers/authController.js
+++ Restaurant-Booking-Management-Application/backend/src/controllers/authController.js
@@ -1,6 +1,5 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
