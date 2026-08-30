8fd275ce606f62922315b728bb22f9f829290c12
kanon-inoue <kanon.inoue.83@gmial.com>
2026-08-30 19:09:11 +1000

    Sub Task 9.5: Implement backend logic for retrieving suitable tables

--- Restaurant-Booking-Management-Application/backend/src/routes/tableRoutes.js
+++ Restaurant-Booking-Management-Application/backend/src/routes/tableRoutes.js
@@ -1,23 +1,21 @@
-const express = require('express');
+const express = require('express')
 
 const {
-    createTable,
-    getTables,
-    updateTable,
-    deleteTable,
-    getAvailableTables
-} = require('../controllers/tableController');
+  createTable,
+  getTables,
+  updateTable,
+  deleteTable,
+  getAvailableTables,
+} = require('../controllers/tableController')
 
-const {
-    protect,
-} = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/authMiddleware')
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/', protect, createTable);
-router.get('/', protect, getTables);
-router.put('/:id', protect, updateTable);
-router.delete('/:id', protect, deleteTable);
-router.get('/availability', protect, getAvailableTables);
+router.post('/', protect, createTable)
+router.get('/', protect, getTables)
+router.put('/:id', protect, updateTable)
+router.delete('/:id', protect, deleteTable)
+router.get('/availability', protect, getAvailableTables)
 
-module.exports = router;
+module.exports = router
