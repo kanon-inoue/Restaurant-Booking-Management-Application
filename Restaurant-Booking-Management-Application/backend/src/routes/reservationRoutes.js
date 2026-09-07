f9cd74fe659782d6c93861e5e2c8c412893d0190
kanon-inoue <kanon.inoue.83@gmail.com>
2026-09-07 03:30:14 +0000

    IA1RRMS-142 Retrieve and display pending staff reservations

--- Restaurant-Booking-Management-Application/backend/src/routes/reservationRoutes.js
+++ Restaurant-Booking-Management-Application/backend/src/routes/reservationRoutes.js
@@ -4,14 +4,21 @@ const {
   createReservation,
   getMyReservations,
   updateReservation,
+  getPendingReservations
 } = require('../controllers/reservationController')
 
-const { protect } = require('../middleware/authMiddleware')
+const { protect, authorize, } = require('../middleware/authMiddleware')
 
 const router = express.Router()
 
 router.post('/', protect, createReservation)
 router.get('/', protect, getMyReservations)
 router.put('/:id', protect, updateReservation)
+router.get(
+  '/pending',
+  protect,
+  authorize('staff'),
+  getPendingReservations
+)
 
 module.exports = router
